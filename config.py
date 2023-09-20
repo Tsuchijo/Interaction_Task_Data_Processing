5d0518af3eb6a3fb217cb54d1139cbe2c36c7709
murph_4090ws <TsuchitoriJoel@gmail.com>
2023-09-19 17:05:35 -0700

    added code to assist in hand labelling of interactions

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 successful_trial_path = PROJECT_ROOT + '/data/successful_trials_by_day/'
 media_path = PROJECT_ROOT + '/data/media/'
 marker_path= PROJECT_ROOT + '/data/marker_reference/'
+movement_labels_path = PROJECT_ROOT + '/data/movement_labels/'
 
 # Remote paths
 teamshare = '/mnt/teams/TM_Lab/Arjun Bhaskaran/'
