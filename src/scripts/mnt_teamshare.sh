a2460b30b94fa005e432b50fa0d0c12d32e9ded9
murph_4090ws <TsuchitoriJoel@gmail.com>
2023-10-04 14:11:33 -0700

    updated readme with more instructions on how to set up repo

--- src/scripts/mnt_teamshare.sh
+++ src/scripts/mnt_teamshare.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
-#mount.cifs //files.ubc.ca/team//BNRC/NINC /mnt -o credentials=/workspaces/Arjun_data/teamshare_credentials,uid=1000,gid=1000,sec=ntlmssp
+#mount.cifs //files.ubc.ca/team//BNRC/NINC /mnt -o uid=1000,gid=1000,sec=ntlmssp
 
